c820740ae36fe7b0e27b71bcf165827042b437d4
atanda0x <atandanafiu21@gmail.com>
2023-12-18 13:50:10 -0800

    refactored codebase

--- main.go
+++ main.go
@@ -1,108 +1,31 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
+	"log"
 	"net/http"
-	"time"
 
+	uploadfile "github.com/atanda0x/fileUpload/uploadFile"
 	"github.com/gorilla/mux"
 )
 
-// File information
-type FileInfo struct {
-	Name        string `json:"name"`
-	UploadedAt  string `json:"uploaded_at"`
-	ContentType string `json:"content_type"`
-	Size        int64  `json:"size"`
-}
-
 // In-memory storage map
-var files map[string][]byte
-
-// Upload file handler
-func uploadFile(w http.ResponseWriter, r *http.Request) {
-	// Check for multipart form
-	if err := r.ParseMultipartForm(32 << 20); err != nil {
-		http.Error(w, "Invalid request", http.StatusBadRequest)
-		return
-	}
-
-	// Get uploaded file
-	file, header, err := r.FormFile("file")
-	if err != nil {
-		http.Error(w, "Error reading file", http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
-
-	// Read file contents
-	data, err := io.ReadAll(file)
-	if err != nil {
-		http.Error(w, "Error reading file", http.StatusBadRequest)
-		return
-	}
-
-	// Store file in memory
-	files[header.Filename] = data
-
-	// Respond with success
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(map[string]string{"message": "File uploaded successfully"})
-}
-
-// List files handler
-func listFiles(w http.ResponseWriter, r *http.Request) {
-	var fileInfos []FileInfo
-
-	for name, data := range files {
-		fileInfos = append(fileInfos, FileInfo{
-			Name:        name,
-			UploadedAt:  time.Now().UTC().String(),
-			ContentType: http.DetectContentType(data),
-			Size:        int64(len(data)),
-		})
-	}
-
-	json.NewEncoder(w).Encode(fileInfos)
-}
-
-// Download file handler
-func downloadFile(w http.ResponseWriter, r *http.Request) {
-	filename := mux.Vars(r)["filename"]
-
-	// Check if file exists
-	data, ok := files[filename]
-	if !ok {
-		http.Error(w, "File not found", http.StatusNotFound)
-		return
-	}
-
-	// Set headers
-	w.Header().Set("Content-Type", http.DetectContentType(data))
-	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", filename))
-
-	// Write file data
-	w.WriteHeader(http.StatusOK)
-	io.Copy(w, bytes.NewReader(data))
-}
 
 func main() {
-	files = make(map[string][]byte)
+	// Initialize the in-memory storage map
+	uploadfile.Files = make(map[string][]byte)
 
+	// Create a new router instance
 	router := mux.NewRouter()
 
-	// Upload endpoint
-	router.HandleFunc("/upload", uploadFile)
+	// Define your routes
+	router.HandleFunc("/upload", uploadfile.UploadFile).Methods("POST")
+	router.HandleFunc("/list", uploadfile.ListFiles).Methods("GET")
+	router.HandleFunc("/download/{filename}", uploadfile.DownloadFile).Methods("GET")
 
-	// List files endpoint
-	router.HandleFunc("/files", listFiles)
+	// Specify the port you want to run the server on
+	port := ":8080"
 
-	// Download file endpoint
-	router.HandleFunc("/files/{filename}", downloadFile)
-
-	fmt.Println("Starting server on port 8080")
-	http.ListenAndServe(":8080", router)
+	// Start the HTTP server
+	log.Printf("Server listening on port %s...\n", port)
+	log.Fatal(http.ListenAndServe(port, router))
 }
